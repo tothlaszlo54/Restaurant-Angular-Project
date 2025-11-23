54a37b432ebe878c5916eb100a760f715a422909
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2025-11-23 20:19:30 +0100

    navbar first version

---
+++ src/app/components/home-main/home-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-main',
+  templateUrl: './home-main.component.html',
+  styleUrl: './home-main.component.scss'
+})
+export class HomeMainComponent {
+
+}
