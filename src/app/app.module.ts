7e5fba44ce9b0a99c647600fe373bf193bd8670e
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2025-11-29 19:43:06 +0100

    footer component done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeMainComponent } from './components/HomePage/home-main/home-main.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuMainComponent } from './components/menu-main/menu-main.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, HomeMainComponent],
+  declarations: [AppComponent, NavbarComponent, HomeMainComponent, FooterComponent, MenuMainComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/menu-main/menu-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-main',
+  templateUrl: './menu-main.component.html',
+  styleUrl: './menu-main.component.scss'
+})
+export class MenuMainComponent {
+
+}
