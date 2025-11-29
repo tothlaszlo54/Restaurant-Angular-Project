aa72c6d816d9caa58e06563d379624b7fcbc8186
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2025-11-29 01:09:44 +0100

    carousel done

--- src/app/components/HomePage/home-main/home-main.component.ts
+++ src/app/components/HomePage/home-main/home-main.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home-main',
   templateUrl: './home-main.component.html',
-  styleUrl: './home-main.component.scss'
+  styleUrl: './home-main.component.scss',
 })
 export class HomeMainComponent {
+  @ViewChild('carousel')
+  carousel!: ElementRef<HTMLDivElement>;
 
+  foodImages = [
+    '/assets/images/carousel/food1.jpg',
+    '/assets/images/carousel/food2.jpg',
+    '/assets/images/carousel/food3.jpg',
+    '/assets/images/carousel/food4.jpg',
+    '/assets/images/carousel/food5.jpg',
+    '/assets/images/carousel/food1.jpg',
+    '/assets/images/carousel/food2.jpg',
+    '/assets/images/carousel/food3.jpg',
+    '/assets/images/carousel/food4.jpg',
+    '/assets/images/carousel/food5.jpg',
+  ];
+
+  private scrollAmount = 180;
+
+  scrollLeft() {
+    this.carousel.nativeElement.scrollBy({
+      left: -this.scrollAmount,
+      behavior: 'smooth',
+    });
+  }
+
+  scrollRight() {
+    this.carousel.nativeElement.scrollBy({
+      left: this.scrollAmount,
+      behavior: 'smooth',
+    });
+  }
 }
