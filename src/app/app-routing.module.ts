8835681d3babe7b25a32b9d1534158c348e0585e
tothlaszlo54 <toth.laszlo.1516@gmail.com>
2026-01-08 20:17:06 +0100

    fixing, clean-code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeMainComponent } from './components/HomePage/home-main/home-main.component';
+import { HomeMainComponent } from './components/home-main/home-main.component';
 import { MenuMainComponent } from './components/menu-main/menu-main.component';
 import { ReservationComponent } from './components/reservation/reservation.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeMainComponent } from './components/HomePage/home-main/home-main.component';
+import { HomeMainComponent } from './components/home-main/home-main.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MenuMainComponent } from './components/menu-main/menu-main.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
